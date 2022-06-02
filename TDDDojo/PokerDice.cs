23b3ebe0dcc98daf9ea9599f099477eae4f11a3c
Jerzy Noworolnik <jnoworolnik@gmail.com>
2022-06-02 11:12:37 +0200

    exercise premature end

---
+++ TDDDojo/PokerDice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDDojo
+{
+    public class PokerDice
+    {
+        public string PokerHand(int[] results)
+        {
+            int[] validArray = new int[] { 1, 2, 3, 4, 5, 6 };
+            if(results.Length != 5)
+            {
+                throw new ArgumentException("Wrong length of input array!");
+            }
+
+            foreach (var validItem in validArray)
+            {
+                validArray.Any
+            }
+            
+            return "";
+
+
+        }
+    }
+}
---
+++ TDDDojoTests/PokerDiceTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+using TDDDojo;
+
+namespace TDDDojoTests
+{
+    [TestFixture]
+    public class PokerDiceTests
+    {
+        [Test]
+        public void PokerHandReturnsString()
+        {
+            // Arrange
+            var pokerDice = new PokerDice();
+            int[] results = new int[5];
+
+            // Act
+            var _result = pokerDice.PokerHand(
+                results);
+
+            // Assert
+            Assert.IsInstanceOf<string>(_result);
+        }
+
+        [Test]
+        [TestCase(new int[] { 1, 2, 3, 4})]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6})]
+        [TestCase(new int[] { })]
+        public void PokerHandInvalidArrayException(int[] array)
+        {
+            var pokerDice = new PokerDice();
+            int[] results = array;
+
+            Assert.Throws<ArgumentException>(() => pokerDice.PokerHand(results));
+        }
+
+        [Test]
+        [TestCase(new int[] { 0, 2, 3, 4, 5})]
+        [TestCase(new int[] { 1, 2, 3, 4, 5})]
+        [TestCase(new int[] { 475, 0, 0, 0, 0})]
+        public void PokerHandArrayWrongNumbersException(int[] array)
+        {
+            // Arrange
+            var pokerDice = new PokerDice();
+            int[] results = array;
+
+            // Act
+            var _result = pokerDice.PokerHand(
+                results);
+
+            // Assert
+            Assert.Throws<ArgumentException>(() => pokerDice.PokerHand(results));
+        }
+    }
+}
